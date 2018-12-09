e317417665f8e6f847662be149a2c4fbd014503f
Adrian de Leon <ajdeleon1064@gmail.com>
2018-12-09 01:57:57 -0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CommentList from './CommentList.js'
+import CommentBox from './CommentBox.js'
+
+export default () => {
+  return (
+    <div>
+      <CommentBox />
+      <CommentList />
+    </div>
+  )
+}
