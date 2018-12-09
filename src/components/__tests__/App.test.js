4acac01f2c0e5f762d1dd5d8c01c4ed0b7f3e1cb
Adrian de Leon <ajdeleon1064@gmail.com>
2018-12-09 02:13:07 -0500

    abstract common setup to beforeEach

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -6,14 +6,16 @@ import App from 'components/App'
 import CommentBox from 'components/CommentBox.js'
 import CommentList from 'components/CommentList.js'
 
-it('shows a comment box', () => {
-  const wrapped = shallow(<App />)
+let wrapped
 
+beforeEach(() => {
+  wrapped = shallow(<App />)
+})
+
+it('shows a comment box', () => {
   expect(wrapped.find(CommentBox).length).toEqual(1)
 })
 
 it('shows a comment list', () => {
-  const wrapped = shallow(<App />)
-
   expect(wrapped.find(CommentList).length).toEqual(1)
 })
